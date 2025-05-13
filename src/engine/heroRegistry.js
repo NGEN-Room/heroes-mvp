c271040d074448fda2374cdaa102ecd4e1792eb5
AkaLeemon <l.waymouth@ngenroom.co.nz>
2025-05-13 15:11:28 +1200

    hell yeah

--- src/engine/heroRegistry.js
+++ src/engine/heroRegistry.js
@@ -1,5 +1,6 @@
 export const heroRoster = {
   Thorn: () => import("@/data/heroes/Thorn/export.js"),
   Kaia: () => import("@/data/heroes/Kaia/export.js"),
-  Gladius: () => import("@/data/heroes/Gladius/export.js")
+  Gladius: () => import("@/data/heroes/Gladius/export.js"),
+  magicMan: () => import("@/data/heros/magicMan/export.js"),
 };
