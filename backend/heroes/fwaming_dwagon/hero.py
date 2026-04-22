3ffda01dff97745e6ea02db53008ba82fc4fe207
Te Piha <54650178+Te-Piha@users.noreply.github.com>
2026-04-22 16:01:29 +1200

    Update fwaming dwagon

---
+++ backend/heroes/fwaming_dwagon/hero.py
@@ -0,0 +1,23 @@
+
+
+HERO = {
+    "id": "fwaming_dwagon",
+    "Name": "Fwaming Dragon",
+    "charcater": {
+        "name": "Fwaming Dragon",
+        "age": 12,
+        "class": {"className": "Pyromaniac with a lisp"},
+        "baseStats": {
+            "hp": 75,
+            "mp": 100,
+            "ap": 5
+        },
+        "rawStats": {
+            "brawn": 5,
+            "brain": 14,
+            "speed": 5
+        },
+    "actions": ACTIONS,
+    "spells": SPELLS,
+    }
+}
