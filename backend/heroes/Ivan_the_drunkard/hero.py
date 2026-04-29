4c448fc873dee937f5d6cf8cdd572b615cc0504e
snail_prevailer21 <thesnaild@gmail.com>
2026-04-29 15:18:36 +1200

    fixes

--- backend/heroes/Ivan_the_drunkard/hero.py
+++ backend/heroes/Ivan_the_drunkard/hero.py
@@ -1,7 +1,7 @@
 from backend.heroes.magic_man.actions import ACTIONS
 from backend.heroes.magic_man.spells import SPELLS
 
-hero = {
+HERO = {
     'id': 'Drunkard',
     'name': 'Ivan the drunkard',
     'drunkness': 'drunk',
@@ -9,7 +9,7 @@ hero = {
         'name': 'Ivan Sakrad',
         'age': 36,
         'class': {'className': 'Drunk_Monk'},
-        'baseStats': {'hp': 85, 'mp': 20, 'ap':20},
+        'baseStats': {'hp': 85, 'mp': 12, 'ap':30},
         'rawStats': {'brawn': 11, 'brain': 4, 'speed': 7},
     },
     'actions': ACTIONS,
--- backend/heroes/Ivan_the_drunkard/tests/hero_test.py
+++ backend/heroes/Ivan_the_drunkard/tests/hero_test.py
@@ -1,3 +1,9 @@
-# import hero 
+from hero import HERO
 # def hero_has_name():
 #     assert hero["name"] != ""
+
+def hero_has_health():
+    assert HERO['hp'] > 0
+
+# def drunkard_drunk():
+#     assert HERO['drunkness'] == 'drunk'
